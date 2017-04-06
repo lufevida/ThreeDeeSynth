87f70fc1587e75a3e709b630673d71cc15b47d62
lufevida <luisfelipe@10.136.95.248>
2017-04-06 16:08:03 -0400

    New 3D stuff!!!

--- max/QuaternionConversion.js
+++ max/QuaternionConversion.js
@@ -1,6 +1,6 @@
 // Taken from Jitter3DUtils.js
 autowatch = 1;
-outlets = 2;
+outlets = 3;
 
 // Convert angle/axis rotation to quaternion.
 function axisToQuaternion(a, b, c, d) {
@@ -33,4 +33,39 @@ function quaternionToAxis(a, b, c, d) {
 	axis[2] = quat[1] / sin_a;
 	axis[3] = quat[2] / sin_a;
 	outlet(1, axis);
+}
+
+var stormTrooper = new JitterObject("jit.gl.model", "ctx");
+stormTrooper.file = "stormtrooper.obj";
+stormTrooper.lighting_enable = 1;
+stormTrooper.smooth_shading = 1;
+stormTrooper.material_mode = 0;
+stormTrooper.color = [0.5, 0, 0.5, 0];
+
+var tree = new JitterObject("jit.gl.model", "ctx");
+tree.file = "trees.obj";
+tree.drawgroup = 5; // draws tree number 5 only
+tree.lighting_enable = 1;
+tree.smooth_shading = 1;
+tree.material_mode = 0;
+tree.color = [0, 1, 0, 1];
+
+var render = new JitterObject("jit.gl.render", "ctx");
+render.erase_color = [1, 1, 1, 1];
+
+var counter = 0;
+
+function draw() {
+	render.erase();
+	render.drawswap();
+	var radians = counter * Math.PI / 180;
+	stormTrooper.position = [Math.cos(radians), Math.sin(radians), 2 * Math.cos(radians) - 4];
+	stormTrooper.rotatexyz = [counter, counter, counter];
+	tree.position = [Math.sin(radians), Math.cos(radians), Math.sin(radians) - 2];
+	tree.rotatexyz = [counter, counter, counter];
+	counter++;
+	counter %= 360;
+	outlet(0, stormTrooper.position); // listener position
+	outlet(1, stormTrooper.quat); // listener rotation
+	outlet(2, tree.position); // sound position
 }
