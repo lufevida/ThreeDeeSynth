61e113e8cdb1214020ae87bdfe79555aa2f89df8
gordonkelliher <gordonkelliher@gmail.com>
2017-04-14 12:57:00 -0400

    body automation/ body control/mic input, etc
    body automation/ body control/mic input, etc

--- max/spherToXYZ.js
+++ max/spherToXYZ.js
@@ -10,12 +10,11 @@ function convert(theta, phi, rho){
 	
 	this.rho = 5;
 	
-	this.phi = Math.atan2(y, x);
+	//this.phi = Math.atan2(y, x);
+	//this.theta = Math.acos(z / rho);
 
-	this.theta = Math.acos(z / rho);
-
-	x = this.rho * Math.sin(theta) * Math.cos(phi);
-	y = this.rho * Math.sin(theta) * Math.sin(phi);
+	x = rho * Math.sin(theta) * Math.cos(phi);
+	y = rho * Math.sin(theta) * Math.sin(phi);
 	z = rho * Math.cos(theta);
 	
 	outlet(0, [x]);
